1e61061a6acfb624100cbb8a80eda53b0f7d4d7b
meowla <redmooncow@163.com>
2022-08-13 13:27:13 +0800

    Updates

---
+++ precache-manifest.de6c90716bb28670ea865cdda14663fb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "59af62143489d3734dcf",
+    "url": "/demo/static/js/main.59af6214.chunk.js"
+  },
+  {
+    "revision": "572d2cf6a7cb6c6e9214",
+    "url": "/demo/static/js/1.572d2cf6.chunk.js"
+  },
+  {
+    "revision": "df776aaf92209c815866",
+    "url": "/demo/static/js/runtime~main.df776aaf.js"
+  },
+  {
+    "revision": "011578b419d6c63e8af9c08052097c09",
+    "url": "/demo/index.html"
+  }
+];
